81a5a68bb669260c995b4e35ebb0acbe98c365b7
Truong Gia Huy <huymindhack@Truongs-MacBook-Air.local>
2025-09-15 12:13:44 +0700

    add video 19

---
+++ src/video18/video18.js
@@ -0,0 +1,20 @@
+//data type
+
+//number
+
+const myAge = 20;
+const myScore = 4.5;
+
+console.log("video 18");
+console.log("My age: " + myAge);
+console.log("My score: " + myScore);
+
+//string
+
+const name = "hoidanit";
+console.log(name);
+
+//boolean
+
+const isDepTrai = true;
+console.log(isDepTrai);
