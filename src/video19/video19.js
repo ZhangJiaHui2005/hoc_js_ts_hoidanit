f7fd3f1be401ff72e1db7bb9e748e1b64d55afaa
Truong Gia Huy <huymindhack@Truongs-MacBook-Air.local>
2025-09-15 12:20:42 +0700

    add video 19

---
+++ src/video19/video19.js
@@ -0,0 +1,18 @@
+// references type
+
+console.log("video 19");
+
+//object: key-value
+
+const person = {
+  name: "Dave Zhang",
+  age: 20,
+  address: "tphcm",
+};
+
+console.log(person);
+
+//array
+
+const scores = [10, 3, 4, 5, 9];
+console.log(scores);
